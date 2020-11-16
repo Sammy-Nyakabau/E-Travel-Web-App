d4838ea80a3756c6cc1f98bf4b1685e93de8d6a8
Sammy Nyakabau <nyakabausammy8@gmail.com>
2020-11-16 17:08:55 +0530

    Rename startup folder to config

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ const debug = require("debug")("app:startup");
 const app = express();
 const port = process.env.PORT || 5000;
 
-require("./startup/cors")(app);
-require("./startup/passport")(app);
-require("./startup/routes")(app);
-require("./startup/db")();
+require("./config/cors")(app);
+require("./config/passport")(app);
+require("./config/routes")(app);
+require("./config/db")();
 
 app.get("/", (req, res) => {
   res.send("hello");
