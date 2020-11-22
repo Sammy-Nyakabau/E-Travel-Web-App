5a4431def2f97cf2879d5eb3e13b204dc9846d56
Sammy Nyakabau <nyakabausammy8@gmail.com>
2020-11-22 16:01:34 +0530

    Move seed.js to scripts folder

---
+++ backend/scripts/seed.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const { Listing } = require("../models/listings");
+const db = process.env.MONGO;
+const key = process.env.API_KEY;
+
+async function seed(pageNumber) {
+  await mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+
+const options = {
+    method: 'GET',
+    url: 'https://mashvisor-api.p.rapidapi.com/airbnb-property/active-listings',
+    params: {state: 'IL', page: pageNumber, city: 'Chicago', items: '20'},
+    headers: {
+      'x-rapidapi-key': key,
+      'x-rapidapi-host': 'mashvisor-api.p.rapidapi.com'
+    }
+  };
+
+
+  const { data: listings } = await axios.request(options);
+
+  await Listing.insertMany(listings.content.properties);
+
+  console.info(`Done with page ${pageNumber}`);
+}
+
+for (let page = 1; page < 99; page++) {
+  seed(page);
+}
