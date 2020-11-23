69c7371caa76176191f42570b21d3b676ee9f4ac
sahilverma2k <sahilverma2k@gmail.com>
2020-11-23 12:45:33 +0530

    recently booked page

--- frontend/src/screens/RecentlyBooked.jsx
+++ frontend/src/screens/RecentlyBooked.jsx
@@ -18,9 +18,10 @@ class RecentlyBooked extends Component {
 
 render() {
     return (
-        <div className="hotel">
-            
+        <div className="recentlybooked">
             <h1 className="recentlybookedlocations">Recently Booked locations:</h1>
+            <div className="booked_card">
+            
            <RecentlyBooked_card style={{ borderbottom:"none"}}
                 img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
                 location="Private room in center of London"
@@ -28,14 +29,14 @@ render() {
                 description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
                 star={4.73}
                 price="£30 / night"
-                total="£117 total"
+                 total="£117 total"
             />
-            <div className="hotel-reviews">
+            <div className="booked_hotel-reviews">
                 
                 <div className="write_review">
                     <Semantic_textarea/>
                 </div>
-                <div className="review">
+                <div className="booked_review">
                     <div className="reviewer_info">
                         <div className="reviewer_photo">
                             <img src="https://a0.muscache.com/im/pictures/user/a13f4039-6f7d-414a-8e11-7778f733fdd7.jpg?im_w=240"/>
@@ -45,6 +46,7 @@ render() {
                     <div className="review_writeup">
                     <p>Location is perfect for a beach get away with amazing, friendly staff to make you feel right at home! Always had things to do everyday and made it natural in making new friends! Definitely another stay for me in the future!</p>
                     </div>
+                </div>
                 </div>
             </div>
             </div>
